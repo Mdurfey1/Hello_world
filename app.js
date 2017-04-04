0f7f97d903241e708a1fe29a1147259baa34813c
Michael <mdurfey@me.com>
2017-04-03 17:56:38 -0700

    added Last.FM API information

--- app.js
+++ app.js
@@ -19,6 +19,24 @@ app.get('/form-group', function(req, res){
   console.log(req)
 });
 
+app.get('/lastFM', function(req, res) {
+var lastFMURL = "https://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&user=almostcrimes_&api_key=cee8eebeb4ff28b14cceee8e805b31b3&period=1month&format=json";
+  request(lastFMURL, function (error, response, body) { 
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    res.send(response.body);
+  })
+})
+
+app.get('/recentTracks', function(req, res){
+var recentTracks = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=almostcrimes_&api_key=cee8eebeb4ff28b14cceee8e805b31b3&format=json"
+request(recentTracks, function (error, response, body) { 
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    res.send(response.body);
+  })
+})
+
 app.listen(port, function () {
 
   console.log('Example app listening on port 3000!');
--- public/javaScript.js
+++ public/javaScript.js
@@ -8,7 +8,34 @@ console.log(wikiTopMargin);
 
 $("#wikiView").css('margin-top', `${wikiTopMargin}`)
 
+$.get("/lastFM", function(d){ 
+  d = JSON.parse(d);
+  console.log(d);
+})
 
+$.get("/recentTracks", function(data){ 
+  data = JSON.parse(data);
+  var dataSet = data.recenttracks.track;
+  console.log(dataSet);
+dataSet.forEach(function (value, index){
+
+  var value = []
+  value += value.push(value);
+
+console.log(value);
+})
+
+value.forEach(function(item) {
+
+  console.log(item);
+  
+})
+
+
+
+
+
+})
 
 
 
