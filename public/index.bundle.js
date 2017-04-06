c74217cf5bc13e6f60353f24efb4e268216ea86b
Michael <mdurfey@me.com>
2017-04-06 12:33:39 -0700

    mobile changes

--- public/index.bundle.js
+++ public/index.bundle.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 
     if (dataSet[0]["@attr"] === undefined) {
       var lastDate = dataSet[0].date["#text"];
-      nowListening += "\n                    <h1 class = 'nowListeningText' style = 'color: rgba(0,0,0, 1);font-size: 21px; font-weight: bold'>\n                    What i've been listening to<i class = \"fa fa-music\"></i>\n                    </h1>\n                    <h3 class = 'notListeningText' style = 'color: white; font-size: 10px; margin-top: -9px;'><span style = \"color: black\"> Updated:</span><br><span style = \"rgba(147,147,147, 1)\">" + lastDate + "</span> </h3>\n                    <h1 class ='notListeningText' style = 'color: white; font-size: 15px; margin-top: -7px;'>\n                      <span style = \"color: black\">Last Track: </span><br>" + lastTrack + "<br>\n                      " + lastArtist + "\n                    </h1> \n                    <img class =\"lastAlbum\" id = \"lastAlbum\" src = \"" + lastImg + "\" style = \"margin-bottom: 15px\"></img>                  \n                  ";
+      nowListening += "\n                    <h1 class = 'nowListeningText' style = 'color: grey;font-size: 21px; font-weight: bold'>\n                    What i've been listening to<i class = \"fa fa-music\"></i>\n                    </h1>\n                    <h3 class = 'notListeningText' style = 'color: white; font-size: 10px; margin-top: -9px;'><span style = \"color: black\"> Updated:</span><br><span style = \"rgba(147,147,147, 1)\">" + lastDate + "</span> </h3>\n                    <h1 class ='notListeningText' style = 'color: white; font-size: 15px; margin-top: -7px;'>\n                      <span style = \"color: black\">Last Track: </span><br>" + lastTrack + "<br>\n                      " + lastArtist + "\n                    </h1> \n                    <img class =\"lastAlbum\" id = \"lastAlbum\" src = \"" + lastImg + "\" style = \"margin-bottom: 15px\"></img>                  \n                  ";
     } else if (dataSet[0]["@attr"].nowplaying === "true") {
       nowListening += "\n              <h1 class = 'nowListeningText' style = 'color: rgba(0,0,0, 1); font-size: 24px; font-weight: bold'>\n                Now Listening<i class = \"fa fa-music\"></i>\n              </h1>\n              <p class = 'trackText' style = 'color: white; font-size: 14px'>\n                " + lastTrack + "\n              </p>\n              <p class = 'trackText' style = 'color: black; margin-top: -10px'>\n                by\n              </p>\n              <h1 class = 'nowListening Text' style = 'color: white; font-size: 18; margin-top: -15px'>\n                      " + lastArtist + "\n              </h1>\n              <img class = 'lastImg' id = 'lastImg' src = '" + lastImg + "' alt = 'Picture not available :('></img>\n              <p class = 'resultText' style = 'color: white; font-size: 14px' id = 'resultText'>\n                " + lastalbum + "\n              </p>\n            ";
     }
@@ -36,7 +36,7 @@ $(document).ready(function () {
         images += "\n\n<img class = \"albumImages\" id = \"albumImages\" src = \"" + data.topalbums.album[i].image[3]["#text"] + "\" alt = \"album Images\"></img>\n\n";
       }
 
-      $("#images").html("<h1 class = \"topAlbumText\" id = \"topAlbumText\">My top albums this past Month:</h1>" + images);
+      $("#images").html("<h1 class = \"topAlbumText\" id = \"topAlbumText\" style = \"color: rgba(100,100,100, 1); font-family: raleyway-thin; font-weight: thin;\">Top albums</h1>" + images);
     });
   });
 });
