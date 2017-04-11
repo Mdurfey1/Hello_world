970e0704bed523f5b683e6c57541d349668fa817
Michael <mdurfey@me.com>
2017-04-11 14:17:06 -0700

    more changes

--- public/javaScript.js
+++ public/javaScript.js
@@ -88,8 +88,8 @@ images += `
   <div class = "image text-center center-block" id = "image">
     <img class = "albumImages img-responsive" id = "albumImages" src = "${data.topalbums.album[i].image[3]["#text"]}" alt = "album Images"></img>
       <div class = "imageFace" onclick = "window.open('${url}')">
-        <h2 class = "artistName" style = "margin: 5px; font-size: 150%; color: white">${artistName}</h2><br>
-        <h2 class = "albumName" style = 'padding: 2px; font-size: 100%; font-family: Raleway; color: white'>${albumName}</h2>
+        <h2 class = "artistName" style = "padding-right: 5%; padding-left: 5%; font-size: 100%; color: white; width: 100%; font-weight: bold">${artistName}</h2>
+        <h2 class = "albumName" style = 'padding-left: 5%; padding-right: 5%; font-size: 80%; font-family: Raleway; color: white; width: 100%;'>${albumName}</h2>
       </div>
   </div>
         `
