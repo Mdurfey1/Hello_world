96311a95bfcfaffcfab9632dee2656b39f699fa4
Michael <mdurfey@me.com>
2017-04-13 13:29:18 -0700

    babel setup complete

--- public/index.bundle.js
+++ public/index.bundle.js
@@ -2,6 +2,12 @@
 
 $(document).ready(function () {
 
+  if ($('.navbar-collapse').hasClass('collapse') === true) {
+    $('nav button').on('click', function () {
+      $('.collapse').toggle();
+      // $('.navbar-collapse').removeClass('.collapse')
+    });
+  }
 
   $.get("/lastFM", function (d) {
     d = JSON.parse(d);
@@ -16,28 +22,48 @@ $(document).ready(function () {
     var lastalbum = dataSet[0].album["#text"];
     var lastImg = dataSet[0].image[3]["#text"];
     var lastTrack = dataSet[0].name;
-    var nowListening = "";
+    var lastURL = dataSet[0].url;
+    var listening = "";
     var images = "";
 
     if (dataSet[0]["@attr"] === undefined) {
       var lastDate = dataSet[0].date["#text"];
-      nowListening += '\n                    <h1 class = \'nowListeningText\' style = \'color: grey;font-size: 21px; font-weight: bold\'>\n                    What i\'ve been listening to<i class = "fa fa-music"></i>\n                    </h1>\n                    <h3 class = \'notListeningText\' style = \'color: white; font-size: 10px; margin-top: -9px;\'><span style = "color: black"> Updated:</span><br><span style = "rgba(147,147,147, 1)">' + lastDate + '</span> </h3>\n                    <h1 class =\'notListeningText\' style = \'color: white; font-size: 15px; margin-top: -7px;\'>\n                      <span style = "color: black">Last Track: </span><br>' + lastTrack + '<br>\n                      ' + lastArtist + '\n                    </h1> \n                    <img class ="lastAlbum" id = "lastAlbum" src = "' + lastImg + '" style = "margin-bottom: 15px"></img>                  \n                  ';
+      console.log("lastDate:" + lastDate);
+      var options = {
+        hour12: true,
+        hour: 'numeric',
+        minute: 'numeric',
+        weekday: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        timeZone: "America/Los_Angeles",
+        timeZoneName: 'short'
+      };
+
+      var timeString = lastDate.toLocaleString(options);
+      console.log("timeString:" + timeString);
+      listening += '\n                    <h1 class = \'nowListeningText\' style = \'color: white;font-size: 21px; font-variant: small-caps; font-family: Raleway; text-shadow: 2px 2px 3px #000\'>\n                    What i\'ve been listening to<i class = "fa fa-music text-primary"></i>\n                    </h1>\n                    <h3 class = \'notListeningText\' style = \'color: white; font-size: 10px; margin-top: -9px;\'><span style = "color: black; text-shadow: 2px 2px 3px #fff; font-family: \'Oswald\';"> Updated:</span><br><span style = "rgba(147,147,147, 1);text-shadow: 2px 2px 3px #000; font-family: \'Raleway\'; font-size: 12px;">' + timeString + '</span> </h3>\n                    <h1 class =\'notListeningText\' style = "color: white; font-size: 20px; font-weight: bold; margin-top: -7px; text-shadow: 2px 2px 3px #000; font-family: \'Raleway\'; font-variant: small-caps;">\n                      <span style = "color: black; text-shadow: 2px 2px 3px #fff; font-family: \'Oswald\'; font-size: 12px">Last Track: </span><br>' + lastTrack + '<br>\n                      ' + lastArtist + '\n                    </h1>                  \n                  <div class = "lastAlbumImage text-center center-block" id = "lastAlbumImage">\n                    <img class ="lastAlbum" id = "lastAlbum" src = "' + lastImg + '"></img>                  \n                    <div class = "lastAlbumImageFace" onclick = "window.open(\'' + lastURL + '\')">\n                      <h2 class = "lastArtistName" style = "padding-right: 5%; padding-left: 5%; padding-top: 10%; font-size: 160%; color: white; width: 100%; font-family: \'Raleway\'; font-weight: bold;">' + lastArtist + '</h2>\n                      <h2 class = "lastAlbumName" style = "padding-left: 5%; padding-right: 5%; padding-top: 5%; font-size: 135%; font-family: \'Raleway\'; color: white; width: 100%; font-weight: bold;">' + lastalbum + '</h2>\n                    </div>\n                  </div>\n    \n                  ';
     } else if (dataSet[0]["@attr"].nowplaying === "true") {
-      nowListening += '\n              <h1 class = \'nowListeningText\' style = \'color: rgba(0,0,0, 1); font-size: 24px; font-weight: bold\'>\n                Now Listening<i class = "fa fa-music"></i>\n              </h1>\n              <p class = \'trackText\' style = \'color: white; font-size: 14px\'>\n                ' + lastTrack + '\n              </p>\n              <p class = \'trackText\' style = \'color: black; margin-top: -10px\'>\n                by\n              </p>\n              <h1 class = \'nowListening Text\' style = \'color: white; font-size: 18; margin-top: -15px\'>\n                      ' + lastArtist + '\n              </h1>\n              <img class = \'lastImg\' id = \'lastImg\' src = \'' + lastImg + '\' alt = \'Picture not available :(\'></img>\n              <p class = \'resultText\' style = \'color: white; font-size: 14px\' id = \'resultText\'>\n                ' + lastalbum + '\n              </p>\n            ';
+      listening += '\n              <h1 class = \'nowListeningText\' style = \'font-size: 24px; font-weight: bold; color: white; text-shadow: 2px 2px 3px #000\'>\n                Now Listening<i class = "fa fa-music text-primary"></i>\n              </h1>\n              <p class = \'trackText\'>\n                ' + lastTrack + '\n              </p>\n              <p class = \'trackText\' style = \'margin-top: -10px; color: white\'>\n                by\n              </p>\n              <h1 class = \'nowListeningText\'>\n                      ' + lastArtist + '\n              </h1>\n              <p class = \'nowListeningAlbum\' id = \'resultText\'>\n                ' + lastalbum + '\n              </p>\n              <div class = "currentAlbumImage text-center" id = "lastAlbumImage">\n                    <img class = \'currentAlbum\' id = \'currentAlbum\' src = \'' + lastImg + '\' alt = \'Picture not available :(\'></img>\n                    <div class = "currentAlbumImageFace" align="center" onclick = "window.open(\'' + lastURL + '\')">\n                      <h2 class = "currentArtistName">' + lastArtist + '</h2>\n                      <h2 class = "currentAlbumName">' + lastalbum + '</h2>\n                    </div>\n              </div>\n            ';
     }
 
-    $("#recent-tracks").html('' + nowListening);
+    $("#recent-tracks").html('' + listening);
 
     $.get('/recentAlbums', function (data) {
       data = JSON.parse(data);
       console.log(data);
       var images = "";
       for (var i = 0; i <= 9; i++) {
+        var url = data.topalbums.album[i].url;
+        var artistName = data.topalbums.album[i].artist.name;
+        var albumName = data.topalbums.album[i].name;
+        var topAlbumText = "";
 
-        images += '\n\n<img class = "albumImages" id = "albumImages" src = "' + data.topalbums.album[i].image[3]["#text"] + '" alt = "album Images"></img>\n\n';
+        topAlbumText += ' <h1 class = "topAlbumText" id = "topAlbumText">Top albums this month:</h1> \n          ';
+        images += '\n\n  <div class = "image text-center" id = "image">\n    <img class = "albumImages img-responsive" id = "albumImages" src = "' + data.topalbums.album[i].image[3]["#text"] + '" alt = "album Images"></img>\n      <div class = "imageFace" onclick = "window.open(\'' + url + '\')">\n        <h2 class = "artistName">' + artistName + '</h2>\n        <h2 class = "albumName">' + albumName + '</h2>\n      </div>\n  </div>\n        ';
       }
-
-      $("#images").html('<h1 class = "topAlbumText" id = "topAlbumText" style = "color: rgba(100,100,100, 1); font-family: raleyway-thin; font-weight: thin;">Top albums</h1>' + images);
+      $(".topAlbumText").html('' + topAlbumText);
+      $(".images").html('' + images);
     });
   });
 });
