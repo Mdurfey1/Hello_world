9a0f6bd4271f958f7389bd96586b26fd73d76570
Michael <mdurfey@me.com>
2017-04-17 23:55:53 -0700

    styling, rank feature, local time zone, and nodemailer

--- public/javaScript.js
+++ public/javaScript.js
@@ -1,4 +1,5 @@
 $(document).ready(function (){
+ 
 
 if ($('.navbar-collapse').hasClass('collapse') === true) {
   $('nav button').on('click', () => { 
@@ -25,43 +26,26 @@ $.get("/recentTracks", function(data){
   var images = "";
 
   if (dataSet[0]["@attr"] === undefined) { 
-var lastDate = dataSet[0].date["#text"];
-    function parseISOLocal(s) {
-  var b = s.split(" ");
-  var hours = b[3].split(":");
-  console.log(b);
-console.log(hours[1]-7)
-console.log(b[0], b[1], b[2], Number(b[3])-700);
-}
-parseISOLocal(lastDate);
-console.log(lastDate)
-    var options = { 
-        hour: 'numeric',
-        minute: 'numeric',
-        weekday: 'short',
-        day: 'numeric',
-        month: 'short',
-        day: 'numeric',
-        localeMatcher: 'lookup'
-        // timeZone: "lookup",
-        // hour12: true
-    }
-   var timeString = lastDate.toLocaleString('en-US', options)
+    var lastDate = dataSet[0].date["#text"];
+    var offset = new Date().getTimezoneOffset();
+    var a = moment(Date.parse(lastDate)).subtract(offset, 'minutes');
+    var timeString = a.format("ddd, MMM Do YYYY, h:mm a");
+
    console.log("timeString:" + timeString);
     listening += `
                     <h1 class = 'nowListeningText' style = 'color: white;font-size: 21px; font-variant: small-caps; font-family: Raleway; text-shadow: 2px 2px 3px #000'>
                     What i've been listening to<i class = "fa fa-music text-primary"></i>
                     </h1>
-                    <h3 class = 'notListeningText' style = 'color: white; font-size: 10px; margin-top: -9px;'><span style = "color: black; text-shadow: 2px 2px 3px #fff; font-family: 'Oswald';"> Updated:</span><br><span style = "rgba(147,147,147, 1);text-shadow: 2px 2px 3px #000; font-family: 'Raleway'; font-size: 12px;">${timeString}</span> </h3>
+                    <h3 class = 'notListeningText' style = 'color: white; font-size: 14px; margin-top: -9px;'><span style = "color: black; text-shadow: 2px 2px 3px #fff; font-family: 'Oswald';"> Updated:</span><br><span style = "rgba(147,147,147, 1);text-shadow: 2px 2px 3px #000; font-family: 'Raleway'; font-size: 14px;">${timeString}</span> </h3>
                     <h1 class ='notListeningText' style = "color: white; font-size: 20px; font-weight: bold; margin-top: -7px; text-shadow: 2px 2px 3px #000; font-family: 'Raleway'; font-variant: small-caps;">
-                      <span style = "color: black; text-shadow: 2px 2px 3px #fff; font-family: 'Oswald'; font-size: 12px">Last Track: </span><br>${lastTrack}<br>
+                      <span style = "color: black; text-shadow: 2px 2px 3px #fff; font-family: 'Oswald'; font-size: 14px">Last Track: </span><br>${lastTrack}<br>
                       ${lastArtist}
                     </h1>                  
                   <div class = "lastAlbumImage text-center center-block" id = "lastAlbumImage">
                     <img class ="lastAlbum" id = "lastAlbum" src = "${lastImg}"></img>                  
                     <div class = "lastAlbumImageFace" onclick = "window.open('${lastURL}')">
-                      <h2 class = "lastArtistName" style = "padding-right: 5%; padding-left: 5%; padding-top: 10%; font-size: 160%; color: white; width: 100%; font-family: 'Raleway'; font-weight: bold;">${lastArtist}</h2>
-                      <h2 class = "lastAlbumName" style = "padding-left: 5%; padding-right: 5%; padding-top: 5%; font-size: 135%; font-family: 'Raleway'; color: white; width: 100%; font-weight: bold;">${lastalbum}</h2>
+                      <h2 class = "lastArtistName" style = "padding-right: 5%; padding-left: 5%; padding-top: 10%; font-size: 4vh; color: white; width: 100%; font-family: 'Raleway'; font-weight: bold;">${lastArtist}</h2>
+                      <h2 class = "lastAlbumName" style = "padding-left: 5%; padding-right: 5%; padding-top: 5%; font-size: 3vh; font-family: 'Raleway'; color: white; width: 100%; font-weight: bold;">${lastalbum}</h2>
                     </div>
                   </div>
     
@@ -106,7 +90,10 @@ for (var i = 0; i <= 9; i++){
 var url = data.topalbums.album[i].url
 var artistName = data.topalbums.album[i].artist.name
 var albumName = data.topalbums.album[i].name
+var rank = data.topalbums.album[i]["@attr"].rank;
 var topAlbumText = "";
+
+console.log(rank);
 
 topAlbumText += ` <h1 class = "topAlbumText" id = "topAlbumText">Top albums this month:</h1> 
           `
@@ -117,6 +104,7 @@ images += `
       <div class = "imageFace" onclick = "window.open('${url}')">
         <h2 class = "artistName">${artistName}</h2>
         <h2 class = "albumName">${albumName}</h2>
+        <h2 class = "rank">Rank: <br>${rank}</h2>
       </div>
   </div>
         `
