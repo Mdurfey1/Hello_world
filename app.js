9c6020731c450366b5cca562c8b7992dd19d2520
Michael Durfey <mdurfey@me.com>
2018-04-16 18:21:50 -0700

    removed config from node modules

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-require('dotenv').config()
 var express = require('express');
 const nodemailer = require('nodemailer');
 var bodyParser = require('body-parser');
 var app = express();
+require('dotenv').config()
 var helmet = require('helmet')
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(helmet())
